0cbafcaccfb8a549b9d86bc3b5c1541313f435ec
dititalbuilding2026-netizen <dititalbuilding2026@gmail.com>
2026-05-23 01:27:38 +0300

    Add files via upload

---
+++ supabaseClient.js.js
@@ -0,0 +1,6 @@
+// روابط ومفاتيح مشروعك الفعلي في Supabase
+const supabaseUrl = 'https://qfdkuzlndzlydcrwboqi.supabase.co';
+const supabaseKey = 'sb_publishable_Ykb5yxO1rEZmclHIS_-q2g__-aOuFW_';
+
+// إنشاء الاتصال
+const supabase = window.supabase.createClient(supabaseUrl, supabaseKey);
