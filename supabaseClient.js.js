f17e3c94f9eec7ab73be8610eb3f5bb818c17708
dititalbuilding2026-netizen <dititalbuilding2026@gmail.com>
2026-05-23 02:14:21 +0300

    Delete supabaseClient.js.js

--- supabaseClient.js.js
+++
@@ -1,6 +0,0 @@
-// روابط ومفاتيح مشروعك الفعلي في Supabase
-const supabaseUrl = 'https://qfdkuzlndzlydcrwboqi.supabase.co';
-const supabaseKey = 'sb_publishable_Ykb5yxO1rEZmclHIS_-q2g__-aOuFW_';
-
-// إنشاء الاتصال
-const supabase = window.supabase.createClient(supabaseUrl, supabaseKey);
